4a0979c790b5e921a333d2532f28f78b76f6b0bd
annashv <annashivrina@gmail.com>
2024-04-20 16:33:53 +0200

    Added Allure attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -42,6 +42,7 @@ public class TestBase {
     public void tearDown() {
         // Clean up after each test if needed
         closeWebDriver();
+        addAttachments();
     }
 
     protected void openPage(String path) {
